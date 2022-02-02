4b1dee4cf7c7fe78f083cff0d972c4148178a1c5
DIDIER AVOAKA <didieravoaka@DIDIERs-MBP.lan>
2022-02-02 08:40:37 -0500

    updated files

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.mars.find_one()
 
     # Return template and data
     return render_template("index.html", mars=mars_data)
@@ -23,12 +23,13 @@ def home():
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-
+    
     # Run the scrape function
     mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    # mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.mars.update_one({}, {"$set": mars_data}, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
